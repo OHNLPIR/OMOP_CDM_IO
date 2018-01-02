babe8e645a395c070620d3eb1b9bfbc649cdc20d
Andrew Wen <Wen.Andrew@mayo.edu>
2018-01-02 14:14:04 -0600

    Use scroll to retrieve patient ID set instead

--- CDM-EMIRS/src/main/java/org/ohnlp/ir/emirs/controllers/SearchController.java
+++ CDM-EMIRS/src/main/java/org/ohnlp/ir/emirs/controllers/SearchController.java
@@ -10,8 +10,11 @@ import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.sort.FieldSortBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.json.JSONArray;
 import org.ohnlp.ir.emirs.Properties;
@@ -79,15 +82,18 @@ public class SearchController {
             query.setCdmQuery(getCDMObjects(query.getUnstructured()));
         }
         if (query.getStructured() != null && query.getStructured().size() > 0) {
-            SearchResponse resp = client.prepareSearch(properties.getEs().getIndexName())
+            SearchResponse scrollResp = client.prepareSearch(properties.getEs().getIndexName())
+                    .addSort(FieldSortBuilder.DOC_FIELD_NAME, SortOrder.ASC)
+                    .setScroll(new TimeValue(60000))
                     .setQuery(query.getPatientIDFilterQuery())
-                    .setSize(50000)
-                    .execute()
-                    .actionGet();
+                    .setSize(100).get();
             ArrayList<Integer> patientIDs = new ArrayList<>();
-            for (SearchHit hit : resp.getHits()) {
-                patientIDs.add(Integer.valueOf(hit.getId()));
-            }
+            do {
+                for (SearchHit hit : scrollResp.getHits()) {
+                    patientIDs.add(Integer.valueOf(hit.getId()));
+                }
+                scrollResp = client.prepareSearchScroll(scrollResp.getScrollId()).setScroll(new TimeValue(60000)).execute().actionGet();
+            } while(scrollResp.getHits().getHits().length != 0);
             query.setPatientIDFilter(patientIDs);
         }
     }
